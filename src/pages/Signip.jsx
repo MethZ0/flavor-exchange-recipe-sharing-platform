1cea386e8330bf96d3b19e316767cc365647d46a
Methush Anjula <methushanjula01@gmail.com>
2025-04-10 02:12:15 +0530

    Create Signip.jsx

---
+++ src/pages/Signip.jsx
@@ -0,0 +1,13 @@
+import SignupForm from "../components/auth/SignupForm"
+import { Container } from "@mui/material"
+
+const Signup = () => {
+  return (
+    <Container sx={{ py: 4 }}>
+      <SignupForm />
+    </Container>
+  )
+}
+
+export default Signup
+
