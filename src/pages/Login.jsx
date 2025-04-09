9eaf7b2297502a66e8f082d517a9f19411606c90
Methush Anjula <methushanjula01@gmail.com>
2025-04-10 02:12:07 +0530

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import LoginForm from "../components/auth/LoginForm"
+import { Container } from "@mui/material"
+
+const Login = () => {
+  return (
+    <Container sx={{ py: 4 }}>
+      <LoginForm />
+    </Container>
+  )
+}
+
+export default Login
+
